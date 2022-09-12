2caa064f093e36af7bcfeff45000349d1127f37c
Paul Burke <paul@paulburke.co>
2022-09-12 10:10:41 -0400

    Adds svelte-material-ui

--- src/new-post/App.svelte
+++ src/new-post/App.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
     import Editor from '../lib/Editor.svelte';
+    import Button from '@smui/button';
+    import {Label} from '@smui/common';
 
+    /**
+     * Bound to the editor
+     */
     let getMarkdown: () => string;
+
+    /**
+     * Editor prop
+     */
     let defaultValue: string;
 
     const submit = () => {
@@ -10,34 +19,40 @@
         chrome.runtime.sendMessage(chrome.runtime.id, {markdown}, response => {
             console.log(`Page got response`, response);
         });
-    }
+    };
 
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const url = urlParams.get('url');
-    const title = urlParams.get('title');
-    const desc = urlParams.get('desc');
-    console.log(url, title, desc);
+    const parseDefaultValue = () => {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const url = urlParams.get('url');
+        const title = urlParams.get('title');
+        const desc = urlParams.get('desc');
+        console.log(url, title, desc);
 
-    defaultValue = `
-**${title}**\n
-> ${desc}\n
-<${url}>
-`
+        defaultValue = `**${title}**\n> ${desc}\n\n <${url}>`
+    };
+
+    parseDefaultValue();
 </script>
 
 <main class="w-full h-full">
+
   <div class="container mx-auto">
 
-    <div class="flex flex-col">
-
+    <div class="flex flex-col mt-6">
       <Editor bind:getMarkdown {defaultValue} />
+    </div>
+
+    <div class="flex justify-end">
+
+      <Button on:click={submit} variant="raised" class="mt-6">
+        <Label>Submit</Label>
+      </Button>
 
     </div>
 
-    <button on:click={submit}>Submit</button>
-
   </div><!-- container -->
+
 </main>
 
 <style>
