00d0ab6e75eda9d49d5a916b3ab035d07b049731
Paul Burke <paul@paulburke.co>
2022-12-12 21:11:13 -0500

    Uses svelte-local-storage-store

--- src/lib/store/preferences.ts
+++ src/lib/store/preferences.ts
@@ -1,22 +1,8 @@
-import {writable} from "svelte/store";
+import {writable} from "svelte-local-storage-store";
 import type {Writable} from "svelte/store";
 
-const storedDarkMode = localStorage.darkMode;
+export const darkMode: Writable<boolean> = writable('darkMode', false);
 
-export const darkMode: Writable<boolean> = writable(storedDarkMode === 'true');
+export const dispatcherDialogShown: Writable<boolean> = writable('dispatcherDialogShown', false);
 
-darkMode.subscribe((value: boolean) => localStorage.darkMode = String(value));
-
-
-const storedDispatcherDialogShown = localStorage.dispatcherDialogShown;
-
-export const dispatcherDialogShown: Writable<boolean> = writable(storedDispatcherDialogShown === 'true');
-
-dispatcherDialogShown.subscribe((value: boolean) => localStorage.dispatcherDialogShown = String(value));
-
-
-const storedSignAsSelf = localStorage.signAsSelf;
-
-export const signAsSelf: Writable<boolean> = writable(storedSignAsSelf === 'true');
-
-signAsSelf.subscribe((value: boolean) => localStorage.signAsSelf = String(value));
+export const signAsSelf: Writable<boolean> = writable('signAsSelf', false);
