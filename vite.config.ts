d60edf90b6cdbc182487a7f61ad6cb35b6a465d2
Paul Burke <paul@paulburke.co>
2023-03-02 11:42:37 -0500

    Switches to using manifest.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import {defineConfig} from 'vite'
 import {svelte} from '@sveltejs/vite-plugin-svelte'
 import {crx} from '@crxjs/vite-plugin'
-// @ts-ignore
-import manifest from './manifest.json'
+import manifest from './manifest.config'
 
 import nodePolyfills from "rollup-plugin-node-polyfills";
 
