b1c951d2ab9e1c19efcb14de2ce5df2ecbac4a56
Paul Burke <paul@paulburke.co>
2024-04-17 14:51:56 -0400

    v2.0.0-beta.15 (1.9.14)

--- manifest.config.ts
+++ manifest.config.ts
@@ -12,7 +12,7 @@ export default defineManifest((env) => ({
     }`,
     short_name: 'Focalize',
     version,
-    version_name: '2.0.0-beta.14',
+    version_name: '2.0.0-beta.15',
     description: 'Social app built on Lens Protocol with DMs via XMTP',
     permissions: [
         'activeTab',
